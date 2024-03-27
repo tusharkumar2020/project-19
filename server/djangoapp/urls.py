4c5368e0e6fc3a2364a9be37af7332a420c4c54b
Travis Lamberte <travis@lamberte.com>
2024-03-27 09:36:41 -0400

    add code engine code

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,9 +15,18 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
+    # path for get_dealerships
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
     # path for dealer reviews view
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
+
+    # path for reviews from dealer_id
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for get_cars view
     path(route='get_cars', view=views.get_cars, name ='getcars'),
