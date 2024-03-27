9006b3d9ca8a51c088db758c64974d273683d46b
Travis Lamberte <travis@lamberte.com>
2024-03-27 11:12:04 -0400

    finishe building application

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,7 +1,8 @@
+import PostReview from "./components/Dealers/PostReview"
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import Dealers from './components/Dealers/Dealers';
-import Dealer from './components/Dealers/Dealer'
+import Dealer from "./components/Dealers/Dealer"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
       <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/dealer" element={<Dealer/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
